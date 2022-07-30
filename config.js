3abf82ef9890e831100b43d4acce6815342037be
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-07-30 14:17:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -36,8 +36,8 @@ global.wait = '_*𝒔𝒆𝒅𝒂𝒏𝒈 𝒅𝒊 𝒑𝒓𝒐𝒔𝒆𝒔...*_
 global.eror = '_*𝒎𝒂𝒂𝒇 𝒔𝒆𝒓𝒗𝒆𝒓 𝒆𝒓𝒓𝒐𝒓...*_'
 
 //========Url Template Buttons=========//
-global.dtu = '𝑌𝑜𝑢𝑡𝑢𝑏𝑒'
-global.urlnya = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ"
+global.dtu = 'Group'
+global.urlnya = "https://chat.whatsapp.com/KZJwR34vNsK134HcFo8fxv"
 
 //============= callButtons =============//
 global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
